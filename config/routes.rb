91425285209d412dfd04576f7f847fef6b6c7410
Conan Browne <Ventrik101@Gmail.com>
2021-09-02 13:58:11 -0400

    Static page tests added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # Root 
+  root 'static_pages#home'
+  # Static pages
   get 'static_pages/home'
   get 'static_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+
 end
