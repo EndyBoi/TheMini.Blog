0219db29d50eab4ba710083f31c7b0935e0bd45a
Conan Browne <Ventrik101@Gmail.com>
2021-09-23 19:25:05 -0400

    valid user emails, next is passwords.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { minimum: 3, maximum: 42 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email, presence: true, length: { minimum:2, maximum:80 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
 end
