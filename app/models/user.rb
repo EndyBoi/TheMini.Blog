b6f4bf5b2eb72f2347bdc520efcd62122bf487ff
Conan Browne <Ventrik101@Gmail.com>
2021-09-27 14:53:53 -0400

    Added user password and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name, presence: true, length: { minimum: 3, maximum: 42 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
-  validates :email, presence: true, length: { minimum:2, maximum:80 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-
+  validates :email, presence: true, length: { minimum:2, maximum:80 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true 
+  has_secure_password
+  validates :password, presence: true, length: {  minimum: 6 }
 end
